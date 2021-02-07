e695f379e20359ffab4f579f212c9cf07a2dec54
popejb1116 <popejb1116@gmail.com>
2021-02-06 23:53:07 -0500

    Installed expo-location - Printing location.corrds object to screen

--- App.js
+++ App.js
@@ -1,15 +1,38 @@
+import React, {useState, useEffect} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hot Server Check</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+   const [location, setLocation] = useState(null);
+   const [errorMsg, setErrorMsg] = useState(null);
+ 
+   useEffect(() => {
+     (async () => {
+       let { status } = await Location.requestPermissionsAsync();
+       if (status !== 'granted') {
+         setErrorMsg('Permission to access location was denied');
+         return;
+       }
+ 
+       let location = await Location.getCurrentPositionAsync({});
+       setLocation(location);
+     })();
+   }, []);
+ 
+   let text = 'Waiting..';
+   if (errorMsg) {
+     text = errorMsg;
+   } else if (location) {
+     text = JSON.stringify(location.coords);
+   }
+ 
+   return (
+     <View style={styles.container}>
+       <Text style={styles.paragraph}>{text}</Text>
+     </View>
+   );
+ }
 
 const styles = StyleSheet.create({
   container: {
